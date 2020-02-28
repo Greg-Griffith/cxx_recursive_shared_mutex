95f77ffeeaf85312c3641a209da512bc4074f663
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-02-28 05:07:22 -0800

    add lightweight version of rsm that is more performant
    lightweight uses a vector instead of a map to track owners. it does not 
    keep a int count for  how many times the owner has locked. instead there 
    are multiple entries in the vector.

---
+++ lib/lightweight_recursive_shared_mutex.cpp
@@ -0,0 +1,213 @@
+// Copyright (c) 2019 Greg Griffith
+// Copyright (c) 2019 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "include/lightweight_recursive_shared_mutex.h"
+
+////////////////////////
+///
+/// Private Functions
+///
+
+bool lightweight_recursive_shared_mutex::end_of_exclusive_ownership()
+{
+    return (_shared_while_exclusive_counter == 0 && _write_counter == 0);
+}
+
+bool lightweight_recursive_shared_mutex::check_for_write_lock(const std::thread::id &locking_thread_id)
+{
+    return (_write_owner_id == locking_thread_id);
+}
+
+bool lightweight_recursive_shared_mutex::check_for_write_unlock(const std::thread::id &locking_thread_id)
+{
+    if (_write_owner_id != locking_thread_id)
+    {
+        return false;
+    }
+#ifdef RSM_DEBUG_ASSERTION
+    if (_shared_while_exclusive_counter == 0)
+    {
+        throw std::logic_error("can not unlock_shared more times than we locked for shared ownership while holding "
+                               "exclusive ownership");
+    }
+#endif
+    return true;
+}
+
+bool lightweight_recursive_shared_mutex::already_has_lock_shared(const std::thread::id &locking_thread_id)
+{
+    auto it = std::find(_read_owner_ids.begin(), _read_owner_ids.end(), locking_thread_id);
+    return (it != _read_owner_ids.end());
+}
+
+void lightweight_recursive_shared_mutex::lock_shared_internal(const std::thread::id &locking_thread_id)
+{
+    _read_owner_ids.emplace_back(locking_thread_id);
+}
+
+void lightweight_recursive_shared_mutex::unlock_shared_internal(const std::thread::id &locking_thread_id)
+{
+    auto it = std::find(_read_owner_ids.begin(), _read_owner_ids.end(), locking_thread_id);
+    if (it != _read_owner_ids.end())
+    {
+        _read_owner_ids.erase(it);
+    }
+#ifdef RSM_DEBUG_ASSERTION
+    else
+    {
+        throw std::logic_error("can not unlock_shared more times than we locked for shared ownership");
+    }
+#endif
+    return;
+
+}
+
+////////////////////////
+///
+/// Public Functions
+///
+
+void lightweight_recursive_shared_mutex::lock()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::unique_lock<std::mutex> _lock(_mutex);
+    if (_write_owner_id == locking_thread_id)
+    {
+        _write_counter++;
+    }
+    else
+    {
+        // Wait until we can set the write-entered.
+        _read_gate.wait(_lock, [this] { return end_of_exclusive_ownership(); });
+
+        _write_counter++;
+        // Then wait until there are no more readers.
+        _write_gate.wait(
+            _lock, [this] { return _read_owner_ids.size() == 0; });
+        _write_owner_id = locking_thread_id;
+    }
+}
+
+bool lightweight_recursive_shared_mutex::try_lock()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::unique_lock<std::mutex> _lock(_mutex, std::try_to_lock);
+
+    if (_write_owner_id == locking_thread_id)
+    {
+        _write_counter++;
+        return true;
+    }
+    else if (_lock.owns_lock() && end_of_exclusive_ownership() && _read_owner_ids.size() == 0)
+    {
+        _write_counter++;
+        _write_owner_id = locking_thread_id;
+        return true;
+    }
+    return false;
+}
+
+void lightweight_recursive_shared_mutex::unlock()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::lock_guard<std::mutex> _lock(_mutex);
+    // you cannot unlock if you are not the write owner so check that here
+    // this might be redundant with the mutex being locked
+    if (_write_counter == 0 || _write_owner_id != locking_thread_id)
+    {
+#ifdef RSM_DEBUG_ASSERTION
+        throw std::logic_error("unlock(standard logic) incorrectly called on a thread with no exclusive lock");
+#else
+        return;
+#endif
+    }
+    _write_counter--;
+    if (end_of_exclusive_ownership())
+    {
+        // reset the write owner id back to a non thread id once we unlock all write locks
+        _write_owner_id = NON_THREAD_ID;
+        // call notify_all() while mutex is held so that another thread can't
+        // lock and unlock the mutex then destroy *this before we make the call.
+        _read_gate.notify_all();
+    }
+}
+
+void lightweight_recursive_shared_mutex::lock_shared()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::unique_lock<std::mutex> _lock(_mutex);
+    if (check_for_write_lock(locking_thread_id))
+    {
+        _shared_while_exclusive_counter++;
+        return;
+    }
+    if (already_has_lock_shared(locking_thread_id))
+    {
+        lock_shared_internal(locking_thread_id);
+    }
+    else
+    {
+        _read_gate.wait(
+            _lock, [this] { return end_of_exclusive_ownership(); });
+        lock_shared_internal(locking_thread_id);
+    }
+}
+
+bool lightweight_recursive_shared_mutex::try_lock_shared()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::unique_lock<std::mutex> _lock(_mutex, std::try_to_lock);
+    if (check_for_write_lock(locking_thread_id))
+    {
+        _shared_while_exclusive_counter++;
+        return true;
+    }
+    if (already_has_lock_shared(locking_thread_id))
+    {
+        lock_shared_internal(locking_thread_id);
+        return true;
+    }
+    if (!_lock.owns_lock())
+    {
+        return false;
+    }
+    if (end_of_exclusive_ownership())
+    {
+        lock_shared_internal(locking_thread_id);
+        return true;
+    }
+    return false;
+}
+
+void lightweight_recursive_shared_mutex::unlock_shared()
+{
+    const std::thread::id &locking_thread_id = std::this_thread::get_id();
+    std::lock_guard<std::mutex> _lock(_mutex);
+    if (check_for_write_unlock(locking_thread_id))
+    {
+        _shared_while_exclusive_counter--;
+        return;
+    }
+    if (_read_owner_ids.size() == 0)
+    {
+#ifdef RSM_DEBUG_ASSERTION
+        throw std::logic_error("unlock_shared incorrectly called on a thread with no shared lock");
+#else
+        return;
+#endif
+    }
+    unlock_shared_internal(locking_thread_id);
+    if (_write_counter != 0)
+    {
+        if (_read_owner_ids.size() == 0)
+        {
+            _write_gate.notify_one();
+        }
+        else
+        {
+            _read_gate.notify_one();
+        }
+    }
+}
